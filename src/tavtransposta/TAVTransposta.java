2f61aae626696bfba01415f3cfc5047ab85b1c6c
charles <charles.ferreira@aluno.ufabc.edu.br>
2015-07-24 17:05:55 -0300

    Criacao do arff sendo feito da forma reduziza. Ganho de memoria, processamento e tempo de execucao

--- src/tavtransposta/TAVTransposta.java
+++ src/tavtransposta/TAVTransposta.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import reduzarff.ReduzArff;
 import transpor_arff.CreateTAV;
 
 /**
@@ -24,43 +25,39 @@ public class TAVTransposta {
      */
     public static void main(String[] args) {
         CreateTAV ct = new CreateTAV();
-//        ConvertClusterToTAV cc = new ConvertClusterToTAV();
-//        String[] Ns = args[0].length() > 1 ? args[0].split(",") : new String[]{args[0]};
+        try {
+            System.out.println("limpando dados");
+            ct.limpaDados("discover.data", "discover.txt");                        //limpa as TAV
 
-//        for (String N : Ns) {
-            try {
-                System.out.println("limpando dados");
-                ct.limpaDados("discover.data", "discover.txt");                        //limpa as TAV
+            System.out.println("extraindo classes");
+            ct.getClasses("discover.names", "classes.txt");                        //extrai as classes
 
-                System.out.println("extraindo classes");
-                ct.getClasses("discover.names", "classes.txt");                        //extrai as classes
+            System.out.println("transpondo a matriz");
+            Process p = Runtime.getRuntime().exec("Rscript scriptTransposicao.r");  //transpoe a TAV
+            p.waitFor();                                                            //aguarda fim do processo
 
-                System.out.println("transpondo a matriz");
-                Process p = Runtime.getRuntime().exec("Rscript scriptTransposicao.r");  //transpoe a TAV
-                p.waitFor();                                                            //aguarda fim do processo
+            new ReduzArff().reduzArff("Tdiscover.txt", "TRdiscover.txt");
 
-                System.out.println("criando arff com TAV transposta");
-                ct.unirDados("Tdiscover.txt", "discover.arff");                         //cria arff com a tav transposta
+            System.out.println("criando arff com TAV transposta");
+            ct.unirDados("TRdiscover.txt", "discover.arff");                         //cria arff com a tav transposta
 
-                System.out.println("fazendo a clusterizacao");
-                String[] cm2 = {"java", "-cp", "weka.jar", "weka.clusterers.SimpleKMeans", "-N", "10", "-I", "500","-O", "-S", "10", "-t", "discover.arff"};
-                ProcessBuilder builder = new ProcessBuilder(Arrays.asList(cm2));
-                builder.redirectOutput(new File("cluster.txt"));
-                builder.redirectError(new File("error.txt"));
-                Process p2 = builder.start(); // throws IOException
-                p2.waitFor();
+            System.out.println("fazendo a clusterizacao");
+            String[] cm2 = {"java", "-cp", "weka.jar", "weka.clusterers.SimpleKMeans", "-N", "10", "-I", "500", "-O", "-S", "10", "-t", "discover.arff"};
+            ProcessBuilder builder = new ProcessBuilder(Arrays.asList(cm2));
+            builder.redirectOutput(new File("cluster.txt"));
+            builder.redirectError(new File("error.txt"));
+            Process p2 = builder.start(); // throws IOException
+            p2.waitFor();
 
-                System.out.println("excluindo arquivos");
-                Runtime.getRuntime().exec("rm discover.txt");
-                Runtime.getRuntime().exec("rm Tdiscover.txt");
-                Runtime.getRuntime().exec("rm discover.arff");
+            System.out.println("excluindo arquivos");
+            Runtime.getRuntime().exec("rm discover.txt");
+            Runtime.getRuntime().exec("rm Tdiscover.txt");
+            Runtime.getRuntime().exec("rm TRdiscover.txt");
+            Runtime.getRuntime().exec("rm discover.arff");
 
-                //cc.convert("cluster.txt", "tabela" + N + ".arff", new int[]{0});
-                //Runtime.getRuntime().exec("rm classes.txt");
-                //Runtime.getRuntime().exec("rm classesAtributos.txt");
-            } catch (InterruptedException | IOException ex) {
-                Logger.getLogger(TAVTransposta.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (InterruptedException | IOException ex) {
+            Logger.getLogger(TAVTransposta.class.getName()).log(Level.SEVERE, null, ex);
+        }
 //        }
     }
 
